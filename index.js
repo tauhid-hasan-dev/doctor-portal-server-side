be29989de7057f63e9f7fa100feab6d6de31251a
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-11-18 00:19:39 +0300

    access token sent to the ui

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
+var jwt = require('jsonwebtoken');
 
 const port = process.env.PORT || 5000;
 
@@ -73,12 +74,26 @@ async function run() {
         })
 
 
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1d' });
+                return res.send({ accesstoken: token });
+            }
+            console.log(user)
+            res.status(403).send({ accesstoken: '' });
+        })
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             console.log(user);
             const result = await usersCollection.insertOne(user);
             res.send(result)
-        })
+        });
+
+
 
         /**
          * API naming conventions
